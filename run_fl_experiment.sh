71f7cdf5fbc176de33d05257588b2f5f4635d70a
raduiord21 <nemus20@gmail.com>
2026-01-19 15:01:46 +0200

    minor changes

--- run_fl_experiment.sh
+++ run_fl_experiment.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # run_fl_experiment.sh
 
+# Set Python path to include the project root
+export PYTHONPATH="${PYTHONPATH}:$(pwd)"
+
 # Start server in background
 python model_side/federated/server.py --num_rounds 10 --num_clients 3 --local_epochs 3 &
 SERVER_PID=$!
@@ -9,9 +12,9 @@ SERVER_PID=$!
 sleep 5
 
 # Start clients
+python model_side/federated/client.py --client_id 0 --server_address 0.0.0.0:8080 &
 python model_side/federated/client.py --client_id 1 --server_address 0.0.0.0:8080 &
 python model_side/federated/client.py --client_id 2 --server_address 0.0.0.0:8080 &
-python model_side/federated/client.py --client_id 3 --server_address 0.0.0.0:8080 &
 
 # Wait for all processes
 wait
